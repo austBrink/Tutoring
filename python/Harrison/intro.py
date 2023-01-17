164c8a9bb8c4edf8345e77be846fb624e372d8f5
austBrink <austbrink@gmail.com>
2023-01-17 17:58:22 -0600

    more coding talk

--- python/Harrison/intro.py
+++ python/Harrison/intro.py
@@ -22,13 +22,13 @@
 # step 4 display to the screen 
 
 # prompt the user by displaying a message
-print('Please enter the first number in the sum')
-firstNumber = int(input())
-print('please enter a value for the second number')
-secondNumber = int(input())
-# create a new variable for the sum 
-sum = firstNumber + secondNumber 
-print(sum)
+# print('Please enter the first number in the sum')
+# firstNumber = int(input())
+# print('please enter a value for the second number')
+# secondNumber = int(input())
+# # create a new variable for the sum 
+# sum = firstNumber + secondNumber 
+# print(sum)
 
 # python will add two 'strings' if you want it two. It will just put one value after the other. 
 # python will only do math on numbers that have been converted to numbers 
@@ -38,6 +38,40 @@ print(sum)
 # functions 
 # error handling 
 # lists and arrays
+
+# write a program that takes 3 numbers from the user. 
+# that program should add the first 2, and then multiply that sum by the third. 
+
+#step 1 get the 1 number from the user 
+# print('give me variable 1')
+# var1 = int(input())
+# print('give me variable 2')
+# var2 = int(input())
+# print('give me variable 3')
+# var3 = int(input())
+
+# # store the sum of the first 2 in a variable 
+# sum = var1 + var2 
+# finalAnswer = sum * var3 
+
+# print(finalAnswer)
+
+# and, or, not 
+
+# trueOrFalse = True 
+# print(not trueOrFalse)
+
+# if statement
+
+isNewUser = False
+
+# i want to print welcome if the user is new 
+# welcome back if the user is NOT true 
+if(isNewUser):
+    print('Welcome')
+else:
+    print('Welcome Back')
+
 
 
 
