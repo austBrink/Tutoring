a3c1c08d95cb5dec43786a4ce58cd89c2dfb2a1e
austBrink <austbrink@gmail.com>
2023-01-19 17:51:13 -0600

    updates

---
+++ python/Egan/intro.py
@@ -0,0 +1,51 @@
+# cover the basics 
+
+# 1  printing (output). The go to for output! 
+# the print is a function, all functions have paranthesis with an optional an argument inside. 
+# the arg is also called parameter. 
+# print('Hello World');
+# print(1) 
+
+# 2 variables / types  
+# 1 + 1 = 2 
+# 'hello' + 'username' = 'hello username'
+# string (str)
+# 'hello', '0', '2' , '2342fsdfsd*&^@#'
+myName = 'Austin'
+myName = 'Austin' + ' Migchelbrink'
+print(myName)
+myName = 3 
+
+
+# number
+#  floats 3.2 
+#  int 2
+x = 2
+y = 3 
+print(x+y)
+
+# boolean 
+# True 
+# False
+# code block is a specific area of the code that does a specific task in a certain context. 
+
+condition = True
+
+if(condition == True):
+    print("its true")
+
+print('All done with if')
+
+price1 = 2
+price2 = 90
+price3 = 45
+
+total = price1 + price2 + price3 + price1
+print('hello' + "WOrld")
+
+
+# Syntax refers to the arrangement of symbols to convey meaning to the code
+# Semantics  
+
+if(True):
+    print('hello')
