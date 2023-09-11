fb7d6bd85448ddd98886ca82a0a56935307cc040
Austin <43417690+austBrink@users.noreply.github.com>
2023-09-11 18:58:19 -0500

    Create shaneFrancis.py

---
+++ python/shaneFrancis.py
@@ -0,0 +1,33 @@
+# comments are made with the hash. blah
+# VARIABLES: smallest unit of data / memory
+# numbers
+# letters
+# strings, characters, int, doubles
+
+# myVariable = 6
+# myVar = 6
+
+# print(myVariable + myVar)
+
+# userInput = input()
+
+# print("you entered...")
+# print(userInput)
+
+# usersNumber = int(input())
+
+# print(usersNumber + 1)
+
+budget = 300
+
+print("please enter your expense")
+expenses = int(input())
+
+if (expenses == budget):
+    print("you out of monies")
+elif (expenses > budget):
+    print("you are in the red.")
+else:
+    print("safely deducted")
+
+# venv
